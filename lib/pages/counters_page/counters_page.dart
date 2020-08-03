62948c0bcda8a8fe90ebec8fde4136e18b7eb46b
Masayuki Ono <mono0926@gmail.com>
2020-08-03 13:48:51 +0900

    Delete unused id argument, and use _Tile as const and use select

--- lib/pages/counters_page/counters_page.dart
+++ lib/pages/counters_page/counters_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_example/providers/providers.dart';
+import 'package:riverpod_example/util/logger.dart';
 
 import 'counter_provider.dart';
 import 'detail_page.dart';
@@ -29,7 +30,7 @@ class CountersPage extends HookWidget {
             overrides: [
               counterId.overrideWithValue(id),
             ],
-            child: _Tile(id: id),
+            child: const _Tile(),
           );
         },
         separatorBuilder: (context, _) => const Divider(height: 0),
@@ -39,16 +40,12 @@ class CountersPage extends HookWidget {
 }
 
 class _Tile extends HookWidget {
-  const _Tile({
-    Key key,
-    @required this.id,
-  }) : super(key: key);
-
-  final String id;
+  const _Tile({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final id = useProvider(counterId);
+    logger.info('id: $id');
     final counterProvider = counterProviders(id);
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
@@ -59,7 +56,7 @@ class _Tile extends HookWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(
-            '${useProvider(counterProvider.state)}',
+            '${useProvider(counterProvider.state.select((s) => s))}',
             style: Theme.of(context).textTheme.subtitle1,
           ),
           const SizedBox(width: 16),
